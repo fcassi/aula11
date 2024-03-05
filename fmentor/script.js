5d928a5eda5e220ea3d26e8a5a2ac89421eb7472
FELIPE ROBERTO CASSIANO <felipe.rcassiano@senacsp.edu.br>
2024-03-05 11:54:16 -0300

    JS

---
+++ fmentor/script.js
@@ -0,0 +1,6 @@
+let base = Number(prompt('Digite o número da base'))
+let altura = Number(prompt('Digite o número da altura'))
+
+let area = base*altura/2
+
+alert(`A área do triângulo mencionado é de ${area}`)
